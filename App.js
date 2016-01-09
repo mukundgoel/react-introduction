20ef8da4398559f65a7c03ae5308dd02d3707002
“mgoel1” <mgoel@walmartlabs.com>
2016-01-09 15:54:16 -0800

    adding more js

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+// This is a stateful component
 class App extends React.Component {
 	render() {
-		return <div>Hello</div>
+		return (
+			<div>
+				<b>Hello</b>
+				<h1>Hello World from State Component</h1>
+			</div>
+		);
 	}
 }
+
+// This is a stateless component (has no state)
+//const App = () => <h1>Hello World from Stateless Component</h1>
 
 export default App
