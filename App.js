4ba0aeca32cc967c9b0dd5e3e56cb033d35b9a0f
“mgoel1” <mgoel@walmartlabs.com>
2016-01-15 09:42:53 -0800

    react basic mounting example

--- App.js
+++ App.js
@@ -8,67 +8,16 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
 	constructor() {
 		super();
-		this.state = {
-			// set initial state to 0 for all 3 Sliders
-			red: 0,
-			green: 0,
-			blue: 0
-		}
+		this.state = { val: 0 }
 		this.update = this.update.bind(this)
 	}
 	update(e) {
-		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red).value,
-			green: ReactDOM.findDOMNode(this.refs.green).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue).value
-		})
+		this.setState({ val: this.state.val + 1 })
 	}
 	render() {
-		//let txt = this.props.txt;
-		//let cat = this.props.cat;
-		// <h1>{txt} ({cat})</h1>
-		// App is parent React component, and Widget is child component
-		return (
-			<div>
-				<Slider ref='red' update={this.update} />
-				{this.state.red}
-				<br />
-				<Slider ref='green' update={this.update} />
-				{this.state.green}
-				<br />
-				<Slider ref='blue' update={this.update} />
-				{this.state.blue}
-				<br />
-			</div>
-		)
+		console.log('rendering!')
+		return <button onClick={this.update}>{this.state.val}</button>
 	}
 }
 
-// This is a stateless component (has no state)
-//const App = () => <h1>Hello World from Stateless Component</h1>
-
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	txt: 'this is the default txt',
-	cat: 5
-}
-
-class Slider extends React.Component {
-	render() {
-		return (
-			<input type='range'
-				min='0'
-				max='255'
-				onChange={this.props.update} />
-		)
-	}
-}
-
-ReactDOM.render(
-	<App txt='this is the props value'/>,
-	document.getElementById('app')
-);
+export default App
--- main.js
+++ main.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App'
 
 // uncomment this when not loading ReactDOM.render from App.js
-//ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
