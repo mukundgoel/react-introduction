2ab6f939d6aedb77664b591429ea8531ac4ba6fe
“mgoel1” <mgoel@walmartlabs.com>
2016-01-15 11:35:14 -0800

    convert to composable react component

--- App.js
+++ App.js
@@ -9,63 +9,68 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			// set initial state to 0 for all 3 Sliders
 			red: 0,
-			green: 0,
-			blue: 0
 		}
 		this.update = this.update.bind(this)
 	}
 	update(e) {
 		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red).value,
-			green: ReactDOM.findDOMNode(this.refs.green).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue).value
+			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
 		})
 	}
 	render() {
-		//let txt = this.props.txt;
-		//let cat = this.props.cat;
-		// <h1>{txt} ({cat})</h1>
-		// App is parent React component, and Widget is child component
 		return (
 			<div>
-				<Slider ref='red' update={this.update} />
-				{this.state.red}
-				<br />
-				<Slider ref='green' update={this.update} />
-				{this.state.green}
-				<br />
-				<Slider ref='blue' update={this.update} />
-				{this.state.blue}
-				<br />
+				<NumInput 
+					ref='red' 
+					min={0}
+					max={255}
+					step={1}
+					value={+this.state.red} // "+" converts it to a number
+					type='number'
+					label='Red'
+					update={this.update} />
 			</div>
 		)
 	}
 }
 
-// This is a stateless component (has no state)
-//const App = () => <h1>Hello World from Stateless Component</h1>
-
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	txt: 'this is the default txt',
-	cat: 5
-}
-
-class Slider extends React.Component {
+class NumInput extends React.Component {
 	render() {
+		var label = this.props.label !== '' ? <label>{this.props.label} - {this.props.value}</label> : 'No Label'
 		return (
-			<input type='range'
-				min='0'
-				max='255'
-				onChange={this.props.update} />
+			<div>
+				<input 
+					ref='inp'
+					type={this.props.type}
+					min={this.props.min}
+					max={this.props.max}
+					step={this.props.step}
+					value={this.props.value}
+					onChange={this.props.update} />
+				{label}
+			</div>
 		)
 	}
+}
+
+NumInput.propTypes = {
+	min: React.PropTypes.number,
+	max: React.PropTypes.number,
+	step: React.PropTypes.number,
+	val: React.PropTypes.number,
+	label: React.PropTypes.string,
+	update: React.PropTypes.func.isRequired,
+	type: React.PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+	min: 0,
+	max: 0,
+	step: 1,
+	val: 0,
+	label: '',
+	type: 'range'
 }
 
 ReactDOM.render(
